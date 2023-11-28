5617275fc5cda141dafa2f51057c8e5d05198339
smilejsu82 <smilejsu82@gmail.com>
2023-11-28 18:26:17 +0900

    999

--- Assets/Scripts/NetworkHand.cs
+++ Assets/Scripts/NetworkHand.cs
@@ -37,31 +37,31 @@ public class NetworkHand : MonoBehaviour //, IPunObservable
         
     }
 
-    // private void Update()
-    // {
-    //     if (!this.photonView.IsMine)
-    //     {
-    //         this.anim.SetFloat("Flex", this.receiveFlex);
-    //         this.anim.SetInteger("Pose", this.receivePose);
-    //         this.anim.SetFloat("Pinch", this.receivePinch);
-    //     }
-    // }
+    private void Update()
+    {
+        if (!this.photonView.IsMine)
+        {
+            this.anim.SetFloat("Flex", this.receiveFlex);
+            this.anim.SetInteger("Pose", this.receivePose);
+            this.anim.SetFloat("Pinch", this.receivePinch);
+        }
+    }
 
-    // public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    // {
-    //     if (stream.IsWriting)
-    //     {
-    //         stream.SendNext(this.anim.GetFloat("Flex"));
-    //         stream.SendNext(this.anim.GetInteger("Pose"));
-    //         stream.SendNext(this.anim.GetFloat("Pinch"));
-    //     }
-    //     else
-    //     {
-    //         this.receiveFlex = (float)stream.ReceiveNext();
-    //         this.receivePose = (int)stream.ReceiveNext();
-    //         this.receivePinch = (float)stream.ReceiveNext();
-    //         
-    //         Debug.LogFormat("{0}, {1}, {2}", this.receiveFlex, this.receivePose, this.receivePinch);
-    //     }
-    // }
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(this.anim.GetFloat("Flex"));
+            stream.SendNext(this.anim.GetInteger("Pose"));
+            stream.SendNext(this.anim.GetFloat("Pinch"));
+        }
+        else
+        {
+            this.receiveFlex = (float)stream.ReceiveNext();
+            this.receivePose = (int)stream.ReceiveNext();
+            this.receivePinch = (float)stream.ReceiveNext();
+            
+            Debug.LogFormat("{0}, {1}, {2}", this.receiveFlex, this.receivePose, this.receivePinch);
+        }
+    }
 }
