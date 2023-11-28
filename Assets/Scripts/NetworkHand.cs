460a64d7fbd685b631c1fbf3d1a6cfc737655716
smilejsu82 <smilejsu82@gmail.com>
2023-11-28 14:18:20 +0900

    왼손 애니메이션 동기화 테스트 1

--- Assets/Scripts/NetworkHand.cs
+++ Assets/Scripts/NetworkHand.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class NetworkHand : MonoBehaviour
+public class NetworkHand : MonoBehaviour, IPunObservable
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Animator anim;
+    private float receiveFlex;
+    private float receivePose;
+    private float receivePinch;
+    
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (stream.IsWriting)
+        {
+            stream.SendNext(this.anim.GetFloat("Flex"));
+            stream.SendNext(this.anim.GetFloat("Pose"));
+            stream.SendNext(this.anim.GetFloat("Pinch"));
+        }
+        else
+        {
+            this.receiveFlex = (float)stream.ReceiveNext();
+            this.receivePose = (float)stream.ReceiveNext();
+            this.receivePinch = (float)stream.ReceiveNext();
+            
+            Debug.LogFormat("{0}, {1}, {2}", this.receiveFlex, this.receivePose, this.receivePinch);
+        }
     }
 }
