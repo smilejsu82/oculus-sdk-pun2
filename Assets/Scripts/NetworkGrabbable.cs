000a9f9499693e82b9d0c9807d96559a96258b69
smilejsu82 <smilejsu82@gmail.com>
2023-11-29 15:04:50 +0900

    큐브 권한 요청 및 이동 동기화

--- Assets/Scripts/NetworkGrabbable.cs
+++ Assets/Scripts/NetworkGrabbable.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Oculus.Interaction;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
-public class NetworkGrabbable : MonoBehaviour
+public class NetworkGrabbable : MonoBehaviour, IPunOwnershipCallbacks
 {
-    // Start is called before the first frame update
+    public InteractableUnityEventWrapper eventWrapper;
+    public PhotonView pv;    
     void Start()
     {
-        
+        this.eventWrapper.WhenSelect.AddListener(() =>
+        {
+            Debug.Log("<color=lime>Grab</color>");
+            this.pv.RequestOwnership();
+        });    
+        this.eventWrapper.WhenUnselect.AddListener(() =>
+        {
+            Debug.Log("<color=lime>UnGrab</color>");
+        });
+    }
+    void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+    void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
     {
-        
+        Debug.LogFormat("<color=yellow>OnOwnershipRequest: {0}, {1}</color>", targetView.name, requestingPlayer.NickName);
+        this.pv.TransferOwnership(requestingPlayer);
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+    {
+        Debug.LogFormat("<color=yellow>OnOwnershipTransfered: {0}, {1}</color>", targetView.name, previousOwner?.NickName);
+    }
+
+    public void OnOwnershipTransferFailed(PhotonView targetView, Player senderOfFailedRequest)
+    {
+        Debug.LogFormat("<color=yellow>OnOwnershipTransferFailed: {0}, {1}</color>", targetView.name, senderOfFailedRequest.NickName);
     }
 }
